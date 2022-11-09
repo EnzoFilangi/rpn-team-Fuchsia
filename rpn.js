919e9d729f19a2b3292fe2d11c3ac95909eb71d3
Sami-EB <dutinfo123@gmail.com>
2022-11-09 11:58:36 +0100

    3-test-split

--- rpn.js
+++ rpn.js
@@ -1,4 +1,15 @@
 
+function parseRpnExpression(rpnExpression){
+    return rpnExpression.split(" ")
+        // Parse numbers and leave operands as strings
+        .map((elem) => {
+            const parsed = Number.parseInt(elem);
+            return isNaN(parsed) ? elem : parsed;
+        })
+        // Remove empty string if there are some
+        .filter((elem) => elem);
+}
+
 function compute(rpnExpression) {
     if(!rpnExpression){
         return 0;
@@ -8,4 +19,4 @@ function compute(rpnExpression) {
 }
 
 
-module.exports = compute
+module.exports = {compute, parseRpnExpression}
--- tests/rpn.test.js
+++ tests/rpn.test.js
@@ -1,9 +1,8 @@
 const test = require('tape');
-const compute = require('../rpn');
+const {compute, parseRpnExpression} = require('../rpn');
 
 
 test("empty string returns 0", function(t) {
-
     const rpnExpression = "";
 
     t.equal(compute(rpnExpression), 0, "Empty expression should equal zero");
@@ -19,3 +18,21 @@ test("a single number in the expression should equal that number", (t) => {
     t.end();
 })
 
+test("empty string is parsed as an empty table", (t) => {
+    const rpnExpression = "";
+
+    const parsedRpnExpression = parseRpnExpression(rpnExpression)
+    t.equal(typeof parsedRpnExpression, typeof []);
+    t.equal(parsedRpnExpression.length, 0);
+
+    t.end();
+})
+
+/*
+test("two numbers and an operand should give the correct result", (t) => {
+    const rpnExpression = "1 2 +";
+
+    t.equal(compute(rpnExpression), 3, "1 + 2 = 3");
+
+    t.end();
+})*/
